808d565725fc06abd28b26fa5178eb250b2b83b4
EECOLOR <eecolor@qirx.org>
2013-12-14 03:40:12 +0100

    Improved version of Rhino environment. Intergrates better with sbt for `test` and `run` tasks.
    - Tests can now be run using the built sbt in test facilities. To use it
      you need to add a dependency on a Scala.js test framework (for example
      Scala.js jasmine test framework) and add a marker interface 
      (`scala.scalajs.test.Test`) to your test classes (or extend 
      JasmineTest when using the Scala.js jasmine test framework).
    - The default Scala.js test framework and Scala.js test bridge class can
      be changed using sbt settings
    - Provided a reference implementation of the test framework using 
      jasmine
    - Added experimental support for `env.js` in the run and test 
      environement. Just add `env.js` webjar as a dependency your project.
    - Run command now searches for objects that have a `main` method (normal
      sbt behaviour)

---
+++ examples/testing/src/test/scala/ElementCreatorTest.scala
@@ -0,0 +1,29 @@
+import scala.scalajs.js.Dynamic.global
+import scala.scalajs.js
+import scala.scalajs.test.JasmineTest
+
+object ElementCreatorTest extends JasmineTest {
+
+  // ElementCreator expects jquery to be present
+  global.importScripts("jquery.js")
+
+  describe("ElementCreator") {
+
+    it("should be create an element in the body") {
+      val document = global.document
+
+      // create the element
+      ElementCreator.create()
+
+      // jquery would make this easier, but I wanted to
+      // only use pure html in the test itself
+      val body = document.getElementsByTagName("body")
+        .asInstanceOf[js.Array[js.Dynamic]].head
+
+      // the Scala.js DOM API would make this easier
+      expect(body.firstChild.tagName.toString == "H1").toBeTruthy
+      expect(body.firstChild.innerHTML.toString == "Test").toBeTruthy
+    }
+  }
+
+}
